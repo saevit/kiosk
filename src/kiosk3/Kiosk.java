134a9c8df7d8f7a98cb4ea6e6ef86eb942ff9682
saevit <woosaevit@gmail.com>
2025-04-24 15:48:02 +0900

    Lv 3. Kiosk 클래스 생성

---
+++ src/kiosk3/Kiosk.java
@@ -0,0 +1,18 @@
+package kiosk3;
+
+import java.util.List;
+
+public class Kiosk {
+    // 속성
+    List<MenuItem> menuItems;
+
+    // 생성자
+    public Kiosk (List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    // 기능
+    public void start (){
+
+    }
+}
