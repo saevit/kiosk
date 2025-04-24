8117ba1aceaa0bc15f7b29e305fa4b5aae5ed88a
saevit <woosaevit@gmail.com>
2025-04-24 15:58:19 +0900

    Lv 3. Kios 클래스의 start 함수로 전체 순서 제어 관리

--- src/kiosk3/App.java
+++ src/kiosk3/App.java
@@ -2,7 +2,6 @@ package kiosk3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -15,32 +14,6 @@ public class App {
         menuItems.add(new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
 
         Kiosk kiosk = new Kiosk(menuItems);
-
-//        while (true) {
-//            System.out.println("============================");
-//
-//            // 메뉴판 출력
-//            System.out.println("[ SHAKESHACK MENU ]");
-//            for (int i = 0; i < menuItems.size(); i++) {
-//                System.out.println((i + 1) + ". " + menuItems.get(i));
-//            }
-//            System.out.println("0. 종료          | 종료");
-//
-//            // 메뉴 입력 받기
-//            Scanner scanner = new Scanner(System.in);
-//
-//            System.out.print("원하는 메뉴를 입력하십시오: ");
-//            int choice = scanner.nextInt();
-//
-//            // 입력 받은 메뉴에 따른 로직 출력
-//            if (choice == 0) {
-//                System.out.println("프로그램을 종료합니다.");
-//                break;
-//            } else if (choice <= menuItems.size()) {
-//                System.out.println("[선택된 메뉴]" + menuItems.get(choice - 1)); // 메뉴는 1번부터 시작
-//            } else {
-//                System.out.println("존재하지 않는 상품입니다.");
-//            }
-//        }
+        kiosk.start();
     }
 }
--- src/kiosk3/Kiosk.java
+++ src/kiosk3/Kiosk.java
@@ -1,5 +1,8 @@
 package kiosk3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import java.util.List;
 
 public class Kiosk {
@@ -13,6 +16,31 @@ public class Kiosk {
 
     // 기능
     public void start (){
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("============================");
+
+            // 메뉴판 출력
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i = 0; i < menuItems.size(); i++) {
+                System.out.println((i + 1) + ". " + menuItems.get(i));
+            }
+            System.out.println("0. 종료          | 종료");
+
+            // 메뉴 입력 받기
+            System.out.print("원하는 메뉴를 입력하십시오: ");
+            int choice = scanner.nextInt();
+
+            // 입력 받은 메뉴에 따른 로직 출력
+            if (choice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            } else if (choice <= menuItems.size()) {
+                System.out.println("[선택된 메뉴]" + menuItems.get(choice - 1)); // 메뉴는 1번부터 시작
+            } else {
+                System.out.println("존재하지 않는 상품입니다.");
+            }
+        }
     }
 }
