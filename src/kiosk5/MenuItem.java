286e6dc0c4b1899097a20aeb22b52db0bccf8ef7
saevit <woosaevit@gmail.com>
2025-04-25 16:15:10 +0900

    Lv 5. 클래스의 필드에 접근 못하도록 캡슐화

---
+++ src/kiosk5/MenuItem.java
@@ -0,0 +1,21 @@
+package kiosk5;
+
+public class MenuItem {
+    // 속성 - 이름, 가격, 설명
+    private String name;
+    private int price;
+    private String info;
+
+    // 생성자
+    public MenuItem (String name, int price, String info){
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+
+    //기능
+    @Override
+    public String toString() {
+        return String.format("%-12s | W %d | %s", name, price, info);
+    }
+}
