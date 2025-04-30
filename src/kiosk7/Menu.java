ac6658030f85afe3751423fbe6cfaa59ca431b54
saevit <woosaevit@gmail.com>
2025-04-30 17:13:14 +0900

    Lv 7.  Menu의 MenuItem를 출력할 때 stream을 사용하도록 수정

--- src/kiosk7/Menu.java
+++ src/kiosk7/Menu.java
@@ -1,6 +1,7 @@
 package kiosk7;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Menu {
     // 속성 - 메뉴의 카테고리 이름, 메뉴를 담은 리스트
@@ -26,9 +27,9 @@ public class Menu {
 
     // 메뉴 전체 출력
     public void printMenu() {
-        for (int i = 0; i < menu.size(); i++) {
-            System.out.println((i + 1) + ". " + menu.get(i)); // 메뉴는 1번부터 시작
-        }
+        IntStream.range(0, menu.size())
+                .mapToObj(i -> (i + 1) + ". " + menu.get(i)) // 메뉴는 1번부터 시작
+                .forEach(System.out::println);
     }
 
     // 선택한 메뉴만 출력
